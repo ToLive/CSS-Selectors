0bc22e0e1565523e79d00ff87f47ab842bc35b4e
Sergey Grabak <seventyseventh@gmail.com>
2023-07-10 23:37:36 +0200

    feat: add test for checkAnswer

--- rs-css/src/features/levels/api/checkAnswer.test.ts
+++ rs-css/src/features/levels/api/checkAnswer.test.ts
@@ -1,14 +1,54 @@
 import { checkAnswer, levels } from "..";
 
+describe('checkAnswer', () => {
+    it('Return false for invalid input selectors', () => {
+        expect(checkAnswer('not a selector')).toBe(false);
+        expect(checkAnswer('.invalid.selector')).toBe(false);
 
-// Tests that the function sets the current level to the user answer minus level step if the answer is a number within the level range
+        expect(checkAnswer("-1")).toBe(false);
+        expect(checkAnswer(levels.length.toString())).toBe(false);
+    });
 
-// Check invalid selectors and levels more and less then levels length
-it('test_returns_false_for_invalid_input', () => {
-    expect(checkAnswer('not a selector')).toBe(false);
-    expect(checkAnswer('.invalid.selector')).toBe(false);
+    it('Return false if user answer is wrong', () => {
+        const TEST_LEVEL = 0;
 
-    expect(checkAnswer("-1")).toBe(false);
-    expect(checkAnswer(levels.length.toString())).toBe(false);
+        const answer = 'deathstar';
+        const result = checkAnswer(answer, TEST_LEVEL);
+
+        expect(result).toBe(false);
+    });
+
+    it('Return true if user answer is right', () => {
+        const TEST_LEVEL = 0;
+
+        const answer = 'vader';
+        const result = checkAnswer(answer, TEST_LEVEL);
+
+        expect(result).toBe(true);
+    });
+
+    it('Return true on every right selector', () => {
+        const TEST_LEVEL = 2;
+
+        const answer1 = 'vader';
+        const answer2 = 'deathstar vader';
+
+        const result1 = checkAnswer(answer1, TEST_LEVEL);
+        const result2 = checkAnswer(answer2, TEST_LEVEL);
+
+        expect(result1).toBe(true);
+        expect(result2).toBe(true);
+    });
+
 });
 
+describe('Check that preconfig answers are correct', () => {
+    test.each(Array.from({ length: levels.length }, (_, i) => i))('Level %i', () => {
+        const TEST_LEVEL = 0;
+
+        const answer = levels[TEST_LEVEL].selector;
+        const result = checkAnswer(answer, TEST_LEVEL);
+
+        expect(result).toBe(true);
+    });
+});
